51acc65a92a719b61e8cc08c39a2acf928056498
Alex-Eusebio <alexandreeusebio499p@gmail.com>
2022-10-06 18:31:20 +0100

    Install Bootstrap

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<?php include("other/header.html");?>
+<div>
+    <h1>Hello</h1>
+</div>
+</body>
+</html>
