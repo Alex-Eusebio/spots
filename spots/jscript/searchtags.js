5add7ad0e10d3e93529f3deb4f9d4792a58bef20
gig <alexandreeusebio499@gmail.com>
2022-10-12 18:30:35 +0100

    Seach Tags Done

--- spots/jscript/searchtags.js
+++ spots/jscript/searchtags.js
@@ -1,5 +1,7 @@
 function searchTags() {
-    
+    document.getElementsByName("estabCard").forEach(a => {
+        a.style.display = "block";
+    });
     let tagSearch = document.getElementsByName("tags");
     var foundTags = [];
     tagSearch.forEach(a => {
@@ -8,23 +10,44 @@ function searchTags() {
             foundTags.push(a.id);
         }
     });
-
-    let estabs = document.getElementsByName("estab");
-    var foundEstabsTags = [];
-    for(i = 0; i < estabs.length; i++){
-        var haveTag = false;
-        for(j=0; j<foundTags.length; j++){
-            var estabTags = Array.from(document.getElementsByName(estabs[i].id));
-            if (estabTags.find(foundTags[j])){
-                haveTag = true;
-                break;
+    console.log(foundTags);
+    if (foundTags.length > 0){
+        let estabs = document.getElementsByName("estabCard");
+        var foundEstabsTags = [];
+        for(i = 0; i < estabs.length; i++){
+            var haveTag = 0;
+            for(j=0; j<foundTags.length; j++){
+                var estabTags = [];
+                document.getElementsByName(estabs[i].id).forEach(a => {
+                    estabTags.push(a.id);
+                });
+                console.log(estabTags.includes(foundTags[j]));
+                if (estabTags.includes(foundTags[j])){
+                    haveTag++;
+                    if(haveTag == foundTags.length)
+                        break;
+                }
+            }
+            if (haveTag == foundTags.length){
+                foundEstabsTags.push(estabs[i].id);
             }
         }
-        if (haveTag){
-            console.log("have tag");
-            foundEstabsTags.push(estabs[i].id);
-            break;
-        }
-    }
 
+        console.log(foundEstabsTags);
+        document.getElementsByName("estabCard").forEach(a => {
+            console.log(a.id);
+            var hide = true;
+            for(i=0;i<foundEstabsTags.length;i++){
+                if (foundEstabsTags[i] == a.id)
+                    hide = false;
+            }
+            if (hide)
+                a.style.display = "none";
+        });
+        
+    } 
+  }
+
+  document.getElementsByName("tags").onclick = function(){
+    searchTags();
   }
--- spots/site/list.php
+++ spots/site/list.php
@@ -30,16 +30,19 @@
     <div class="collapse" id="collapseExample">
     <div class="btn-group-toggle" data-toggle="badges" id="tagList">
         <label class="badge badge-secondary text-capitalize active">
-            <input type="checkbox" name="tags" id="restaurante" checked> Restaurante
+            <input type="checkbox" name="tags" id="restaurante" onclick="searchTags()"> Restaurante
         </label>
         <label class="badge badge-secondary text-capitalize active">
-            <input type="checkbox" name="tags" id="cafe" checked> Café
+            <input type="checkbox" name="tags" id="cafe" onclick="searchTags()"> Café
         </label>
         <label class="badge badge-secondary text-capitalize active">
-            <input type="checkbox" name="tags" id="cinema"> Cinema
+            <input type="checkbox" name="tags" id="cinema" onclick="searchTags()"> Cinema
         </label>
         <label class="badge badge-secondary text-capitalize active">
-            <input type="checkbox" name="tags" id="agua" checked> Água
+            <input type="checkbox" name="tags" id="agua" onclick="searchTags()"> Água
+        </label>
+        <label class="badge badge-secondary text-capitalize active">
+            <input type="checkbox" name="tags" id="barato" onclick="searchTags()"> Barato
         </label>
     </div>
     </div>
@@ -47,16 +50,16 @@
     <div class="d-flex flex-column">
         <fieldset class="border border-warning destaques" style="margin-bottom: 1rem">
             <legend class="text-left text-capitalize">Destaques</legend>
-            <div class="card mb-3 mx-auto" style="max-width: 55rem;">
+            <div class="card mb-3 mx-auto" style="max-width: 55rem;" id="ud" name="estabCard">
             <div class="row g-0">
                 <div class="col-md-5">
                     <img src="../imgs/1.jpg" class="img-fluid rounded estabImg" alt="...">
                 </div>
                 <div class="col-md-7">
                     <div class="card-body">
-                        <h5 class="card-title" name="estab" id="ud">Restaurante</h5>
+                        <h5 class="card-title" name="estab">Restaurante</h5>
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
-                        <p class="card-text"><a href="#" class="badge badge-secondary text-capitalize" name="ud" id="restaurante">Restaurante</a> <a href="#" class="badge badge-secondary text-capitalize" name="ud" id="agua">Água</a> <a href="#" class="badge badge-secondary text-capitalize">Portugues</a> <a href="#" class="badge badge-secondary text-capitalize">Barato</a> <a href="#" class="badge badge-secondary text-capitalize">Frutos do Mar</a> <a href="#" class="badge badge-secondary text-capitalize">Carne</a> <a href="#" class="badge badge-secondary text-capitalize">Secundario</a></p>
+                        <p class="card-text"><a href="#" class="badge badge-secondary text-capitalize" name="ud" id="restaurante">Restaurante</a> <a href="#" class="badge badge-secondary text-capitalize" name="ud" id="agua">Água</a> <a href="#" class="badge badge-secondary text-capitalize">Portugues</a> <a href="#" class="badge badge-secondary text-capitalize" name="ud" id="barato">Barato</a> <a href="#" class="badge badge-secondary text-capitalize">Frutos do Mar</a> <a href="#" class="badge badge-secondary text-capitalize">Carne</a> <a href="#" class="badge badge-secondary text-capitalize">Secundario</a></p>
                         <p class="card-text"><small class="text-muted">3 mins atras</small> <a class="btn btn-info float-md-right text-capitalize" href="estab.php" role="button">Ver</a></p>
                         
                     </div>
@@ -64,14 +67,14 @@
             </div>
         </div>
         </fieldset>
-        <div class="card mb-3 mx-auto" style="max-width: 55rem;">
+        <div class="card mb-3 mx-auto" style="max-width: 55rem;" id="id" name="estabCard">
             <div class="row g-0">
                 <div class="col-md-5">
                     <img src="../imgs/1.jpg" class="img-fluid rounded estabImg" alt="...">
                 </div>
                 <div class="col-md-7">
                     <div class="card-body">
-                        <h5 class="card-title" name="estab" id="id">Café</h5>
+                        <h5 class="card-title" name="estab">Café</h5>
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
                         <p class="card-text"><a href="#" class="badge badge-secondary text-capitalize" name="id" id="cafe">Café</a><a href="#" class="badge badge-secondary text-capitalize" name="id" id="agua">Água</a></p>
                         <p class="card-text"><small class="text-muted">3 mins atras</small> <a class="btn btn-info float-md-right text-capitalize" href="estab.php" role="button">Ver</a></p>
@@ -79,7 +82,22 @@
                 </div>
             </div>
         </div>
+        <div class="card mb-3 mx-auto" style="max-width: 55rem;" id="pd" name="estabCard">
+            <div class="row g-0">
+                <div class="col-md-5">
+                    <img src="../imgs/1.jpg" class="img-fluid rounded estabImg" alt="...">
+                </div>
+                <div class="col-md-7">
+                    <div class="card-body">
+                        <h5 class="card-title" name="estab">Cinema</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+                        <p class="card-text"><a href="#" class="badge badge-secondary text-capitalize" name="pd" id="cafe">Café</a></p>
+                        <p class="card-text"><small class="text-muted">3 mins atras</small> <a class="btn btn-info float-md-right text-capitalize" href="estab.php" role="button">Ver</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <script>searchTags();</script>
+    <!--<script>searchTags();</script>-->
 </body>
 </html>
